56b89969e25cf58de08e7ac6a1b29b177b18d001
Cody Poll <cjpoll@gmail.com>
2017-07-18 16:59:27 -0600

    Improve testpipeline example

--- lib/rivulet/test_pipeline.ex
+++ lib/rivulet/test_pipeline.ex
@@ -1,24 +1,33 @@
 defmodule Rivulet.TestPipeline do
-  def start_link(topic, partition) do
-    with {:ok, kafka} <- Rivulet.Kafka.Consumer.start_link(topic, partition) do
-      starting(topic, partition, kafka)
-    end
+  use GenServer
+
+  def start_link(topic, partition, offset \\ nil) do
+    GenServer.start_link(__MODULE__, {topic, partition, offset})
   end
 
-  def start_link(topic, partition, offset) do
-    with {:ok, kafka} <- Rivulet.Kafka.Consumer.start_link(topic, partition, offset) do
-      starting(topic, partition, kafka)
-    end
+  def init({topic, partition, nil}) do
+    :ok =
+      with {:ok, kafka} <- Rivulet.Kafka.Consumer.start_link(topic, partition) do
+        starting(topic, partition, kafka)
+      end
+
+    {:ok, {}}
+  end
+
+  def init({topic, partition, offset}) do
+    :ok =
+      with {:ok, kafka} <- Rivulet.Kafka.Consumer.start_link(topic, partition, offset) do
+        starting(topic, partition, kafka)
+      end
+
+    {:ok, {}}
   end
 
   defp starting(topic, partition, kafka) do
-    with {:ok, deserializer} <- Rivulet.Avro.Deserializer.start_link(topic, partition),
-         {:ok, printer} <- Rivulet.EventPrinter.start_link do
-
-        GenStage.sync_subscribe(deserializer, to: kafka)
-        GenStage.sync_subscribe(printer, to: deserializer)
-
-        {:ok, kafka}
+    with {:ok, deserializer} <- Rivulet.Avro.Deserializer.start_link(topic, partition, kafka),
+         {:ok, printer} <- Rivulet.EventPrinter.start_link(deserializer),
+         {:ok, _consumer} <- Rivulet.Pipeline.Consumer.start_link(printer) do
+      :ok
     end
   end
 end
