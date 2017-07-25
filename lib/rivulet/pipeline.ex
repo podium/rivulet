f7b23edc8ad894368eab670d7b8f6e1387bc48fc
Cody Poll <cjpoll@gmail.com>
2017-07-25 13:41:11 -0600

    Make a better abstraction for pipelines

--- lib/rivulet/pipeline.ex
+++ lib/rivulet/pipeline.ex
@@ -1,35 +1,40 @@
 defmodule Rivulet.Pipeline do
-  use Supervisor
+  use GenServer
 
   alias Rivulet.Kafka.Partition
 
-  @type extra_args :: [term]
+  @callback init(Partition.t, GenStage.t) :: :ok | {:error, term}
 
-  @spec start_link(Partition.topic, module, atom, [term])
-  :: Supervisor.on_start
-  def start_link(topic, child_module, rivulet_name, extra_args \\ [])
-  when is_binary(topic)
-  and is_atom(rivulet_name)
-  and is_list(extra_args) do
-    Supervisor.start_link(__MODULE__, {topic, child_module, extra_args}, name: rivulet_name)
+  defmacro __using__(_) do
+    quote do
+      @behaviour Rivulet.Pipeline
+    end
   end
 
-  def init({topic, child_module, extra_args}) when is_binary(topic) do
-    {:ok, partition_count} = Rivulet.Kafka.Partition.partition_count(topic)
-
-    topic
-    |> children(partition_count |> range, child_module, extra_args)
-    |> supervise([strategy: :one_for_one])
+  @spec start_link(module, Partition.t, Partition.offset | nil, GenServer.options)
+  :: GenServer.on_start
+  def start_link(module, %Partition{} = partition, offset \\ nil, opts \\ []) do
+    GenServer.start_link(__MODULE__, {module, partition, offset}, opts)
   end
 
-  defp children(topic, range, child_module, extra_args) do
-    Enum.map(range, fn(partition) ->
-      partition = %Partition{topic: topic, partition: partition}
-      supervisor(child_module, [partition] ++ extra_args, id: "#{child_module}.#{topic}.#{partition.partition}")
-    end)
+  def init({module, %Partition{} = partition, offset}) do
+    resp =
+      with {:ok, kafka} <- kafka(partition, offset)  do
+        module.init(partition, kafka)
+      end
+
+    case resp do
+      :ok -> {:ok, {}}
+      {:error, reason} -> {:stop, reason}
+      _ -> {:stop, :invalid_init_return}
+    end
   end
 
-  defp range(count) when is_integer(count) do
-    0..(count - 1)
+  defp kafka(%Partition{} = partition, nil) do
+    Rivulet.Kafka.Consumer.start_link(partition)
+  end
+
+  defp kafka(%Partition{} = partition, offset) do
+    Rivulet.Kafka.Consumer.start_link(partition, offset)
   end
 end
