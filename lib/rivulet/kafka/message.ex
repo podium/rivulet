c5f69d8964bff86979f06c44ec2a419a5f0c2d6c
Cody Poll <cjpoll@gmail.com>
2017-07-26 11:45:43 -0600

    Also include schemas in Message

--- lib/rivulet/kafka/message.ex
+++ lib/rivulet/kafka/message.ex
@@ -1,13 +1,15 @@
 defmodule Rivulet.Kafka.Message do
-  defstruct attributes: 0, crc: nil, offset: nil, raw_key: nil, raw_value: nil, decoded_key: nil, decoded_value: nil
+  defstruct attributes: 0, crc: nil, offset: nil, raw_key: nil, raw_value: nil, decoded_key: nil, decoded_value: nil, key_schema: nil, value_schema: nil
   @type t :: %__MODULE__{
     attributes: non_neg_integer,
     crc: non_neg_integer,
     offset: non_neg_integer,
     raw_key: binary,
     raw_value: binary,
-    decoded_key: nil | term,
-    decoded_value: nil | term
+    key_schema: Avro.schema | nil,
+    value_schema: Avro.schema | nil,
+    decoded_key: term | nil,
+    decoded_value: term | nil
   }
 
   def from_wire_message(%KafkaEx.Protocol.Fetch.Message{} = msg) do
