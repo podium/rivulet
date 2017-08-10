147d14a8f7a3a1b5bf96d10993ec52a671756d77
Cody Poll <cjpoll@gmail.com>
2017-08-10 16:17:03 -0600

    Add more debug logging

--- lib/rivulet/kafka/consumer.ex
+++ lib/rivulet/kafka/consumer.ex
@@ -39,29 +39,39 @@ defmodule Rivulet.Kafka.Consumer do
   end
 
   def handle_demand(demand, %State{} = state) do
+    Logger.debug("[#{state.partition.topic}][#{state.partition.partition}] Received #{demand} demand")
     {events, %State{} = state} = pull_data(demand, state)
 
     {:noreply, events, state}
   end
 
   def handle_info({:poll, demand}, %State{} = state) do
+    Logger.debug("[#{state.partition.topic}][#{state.partition.partition}]Polling for #{demand} more messages from Kafka")
     {events, %State{} = state} = pull_data(demand, state)
 
     {:noreply, events, state}
+  end
+
+  def handle_info(_, %State{} = state) do
+    {:noreply, state}
   end
 
   # Implementation functions
 
   @spec pull_data(pos_integer, State.t) :: {[Message.t], State.t}
   def pull_data(count, %State{queued: [], offset: offset} = state) do
+    Logger.debug("[#{state.partition.topic}][#{state.partition.partition}] Pulling messages from Kafka")
+
     messages =
       state.partition
       |> fetch(offset)
       |> messages
       |> Enum.map(&Message.from_wire_message/1)
       |> Enum.sort(fn(%Message{} = a, %Message{} = b) ->
-           a.offset >= b.offset
+           a.offset <= b.offset
          end)
+
+    Logger.debug("[#{state.partition.topic}][#{state.partition.partition}] Found #{length(messages)} messages on Kafka")
 
     {events, queued} = Enum.split(messages, count)
 
@@ -69,16 +79,22 @@ defmodule Rivulet.Kafka.Consumer do
 
     if delivered_count < count, do: :timer.send_after(:timer.seconds(1), {:poll, count - delivered_count})
 
-    {events, %State{state | queued: queued}}
+
+    {events, %State{state | queued: queued, offset: new_offset(offset, messages)}}
   end
 
 
   def pull_data(count, %State{queued: queued} = state) do
+    Logger.debug("[#{state.partition.topic}][#{state.partition.partition}] Pulling data from kafka consumer buffer")
     {events, queued} = Enum.split(queued, count)
 
     delivered_count = length(events)
 
-    if delivered_count < count, do: :timer.send_after(:timer.seconds(1), {:poll, count - delivered_count})
+    if delivered_count < count do
+      polling_interval = :timer.seconds(1)
+      :timer.send_after(polling_interval, {:poll, count - delivered_count})
+      Logger.debug("[#{state.partition.topic}][#{state.partition.partition}] Only found #{delivered_count} messages in buffer - will poll for more data in #{polling_interval / 1000} seconds")
+    end
 
     {events, %State{state | queued: queued}}
   end
@@ -88,10 +104,27 @@ defmodule Rivulet.Kafka.Consumer do
   end
 
   defp fetch(%Partition{topic: topic, partition: partition}, nil) do
+    Logger.debug("Fetching from kafka (no offset given)")
     KafkaEx.fetch(topic, partition)
   end
 
   defp fetch(%Partition{topic: topic, partition: partition}, offset) do
+    Logger.debug("Fetching from kafka (offset: #{offset})")
     KafkaEx.fetch(topic, partition, offset: offset)
+  end
+
+  defp new_offset(nil, _messages), do: nil
+  defp new_offset(offset, []) do
+    offset
+  end
+  defp new_offset(_offset, messages) do
+    highest_offset(messages) + 1
+  end
+
+  defp highest_offset(messages) do
+    Enum.reduce(messages, 0, fn
+      (%Message{offset: offset}, acc) when offset > acc  -> offset
+      (_, acc) ->  acc
+    end)
   end
 end
