1b84b478a056ab6ad8ada3dcfb2232d28794973e
Cody Poll <cjpoll@gmail.com>
2017-09-28 10:40:45 -0600

    Clean up compile warnings

--- lib/rivulet/application.ex
+++ lib/rivulet/application.ex
@@ -22,17 +22,21 @@ defmodule Rivulet.Application do
     config = Application.get_all_env(:rivulet)
 
     config =
-      if Keyword.get(dynamic_hosts) do
+      if Keyword.get(config, :dynamic_hosts) do
         hosts =
-          case hosts = System.get_env("KAFKA_HOSTS") do
+          case System.get_env("KAFKA_HOSTS") do
             nil -> Logger.error("KAFKA_HOSTS not set")
             value -> kafka_hosts(value)
           end
 
-        Enum.each(config, fn({k, v}) ->
-          Application.put_env(:kafka_ex, k, v, persistent: true)
-        end)
+        Keyword.put(config, :brokers, hosts)
+      else
+        config
       end
+
+    Enum.each(config, fn({k, v}) ->
+      Application.put_env(:kafka_ex, k, v, persistent: true)
+    end)
 
     unless Mix.env == :test do
       Application.ensure_all_started(:kafka_ex)
@@ -43,12 +47,12 @@ defmodule Rivulet.Application do
     string
     |> String.split(",")
     |> Enum.map(fn(host_string) ->
-         uri = %URI{} = URI.parse(host_string)
          case String.split(host_string, ":") do
            [host, port] ->
              {host, String.to_integer(port)}
            [host] ->
              {host, 9092}
+         end
        end)
   end
 end
