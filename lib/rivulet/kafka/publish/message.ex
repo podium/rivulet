28098d75c8c44c915b09b5dbff0c3afece8702f6
Cody Poll <cjpoll@gmail.com>
2017-10-12 11:56:00 -0600

    Dialyzer fix

--- lib/rivulet/kafka/publish/message.ex
+++ lib/rivulet/kafka/publish/message.ex
@@ -2,6 +2,8 @@ defmodule Rivulet.Kafka.Publish.Message do
   @enforce_keys [:topic, :partition_strategy, :encoding_strategy, :value]
   defstruct [:topic, :partition, :partition_strategy, :encoding_strategy, :key, :value]
 
+  alias Rivulet.Kafka.Publisher
+
   @type t :: %__MODULE__{
     topic: Partition.topic,
     partition: Partition.partition,
