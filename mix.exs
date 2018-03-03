29409fbf65d97efb8d2a32efd4ee52000b1dc016
Cody J. Poll <cjpoll@gmail.com>
2018-03-03 10:23:24 -0700

    Update avro_ex version

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Rivulet.Mixfile do
 
   defp deps do
     [{:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
-     {:avro_ex, "~> 0.1.0-beta.2"},
+     {:avro_ex, "~> 0.1.0-beta.3"},
      {:brod, "~> 3.3.1"},
      {:httpoison, ">= 0.12.0"},
      {:meck, "~> 0.8.7", only: [:test]},
