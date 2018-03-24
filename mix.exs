8bc4196a015a9dcf4c2888949c80cea5506103e4
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2018-03-24 11:40:32 -0600

    update

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule Rivulet.Mixfile do
 
   defp deps do
     [{:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
-     {:avro_ex, "~> 0.1.0-beta.5"},
+     # {:avro_ex, "~> 0.1.0-beta.5"},
+     {:avro_ex, git: "git@github.com:dconger/avro_ex", ref: "master"},
      {:brod, "~> 3.3.1"},
      {:httpoison, ">= 0.12.0"},
      {:meck, "~> 0.8.7", only: [:test]},
