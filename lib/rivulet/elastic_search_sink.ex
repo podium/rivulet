36271dace207704b552c428cd697ccb00a4a7e59
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2018-04-03 16:54:37 -0500

    updated

--- lib/rivulet/elastic_search_sink.ex
+++ lib/rivulet/elastic_search_sink.ex
@@ -1,51 +1,86 @@
 defmodule Rivulet.ElasticSearchSink do
-  require Logger
+  @type t :: module
 
-  alias Rivulet.Kafka.Partition
-  alias Rivulet.ElasticSearchSink.Database.ElasticSearchGenerator
+  @doc false
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      @behaviour Rivulet.ElasticSearchSink
 
-  defdelegate start_link(opts), to: Rivulet.ElasticSearchSink.Supervisor
+      {otp_app, config} = Rivulet.ElasticSearchSink.Supervisor.compile_config(__MODULE__, opts)
+      @otp_app otp_app
+      @config  config
 
-  defmodule Config do
-    defstruct [
-      :consumer_group,
-      :elastic_index,
-      :elastic_url,
-      :elastic_type,
-      :elastic_mapping,
-      :topic,
-    ]
+      def config do
+        {:ok, config} = Rivulet.ElasticSearchSink.Supervisor.runtime_config(:dry_run, __MODULE__, @otp_app, [])
+        config
+      end
 
-    @type t :: %__MODULE__{
-      consumer_group: String.t,
-      elastic_index: String.t,
-      elastic_url: String.t,
-      elastic_type: String.t,
-      elastic_mapping: map,
-      topic: Partition.topic
-    }
+      def child_spec(opts) do
+        %{
+          id: __MODULE__,
+          start: {__MODULE__, :start_link, [opts]},
+          type: :supervisor
+        }
+      end
 
-    def from_sink_opts(opts) do
-      consumer_group = Keyword.fetch!(opts, :consumer_group)
-      elastic_index = Keyword.get(opts, :elastic_index)
-      elastic_url = Keyword.get(opts, :elastic_url, [])
-      elastic_type = Keyword.fetch!(opts, :elastic_type)
-      topic = Keyword.fetch!(opts, :topic)
-      elastic_mapping = Keyword.get(opts, :elastic_mapping)
+      def start_link(opts \\ []) do
+        Rivulet.ElasticSearchSink.Supervisor.start_link(__MODULE__, @otp_app, @adapter, opts)
+      end
 
-      %__MODULE__{
-        consumer_group: consumer_group,
-        elastic_index: elastic_index,
-        elastic_url: elastic_url,
-        elastic_type: elastic_type,
-        topic: topic,
-        elastic_mapping: elastic_mapping
-      }
+      def stop(pid, timeout \\ 5000) do
+        Supervisor.stop(pid, :normal, timeout)
+      end
+
+      defoverridable child_spec: 1
     end
   end
 
-  def ensure_es_setup!(%Config{} = config) do
-    ElasticSearchGenerator.create_index(config)
-    ElasticSearchGenerator.create_mapping(config)
-  end
+  @optional_callbacks init: 2
+
+  @doc """
+  Returns the adapter configuration stored in the `:otp_app` environment.
+
+  If the `c:init/2` callback is implemented in the repository,
+  it will be invoked with the first argument set to `:dry_run`.
+  """
+  @callback config() :: Keyword.t
+
+  @doc """
+  Starts any connection pooling or supervision and return `{:ok, pid}`
+  or just `:ok` if nothing needs to be done.
+
+  Returns `{:error, {:already_started, pid}}` if the repo is already
+  started or `{:error, term}` in case anything else goes wrong.
+
+  ## Options
+
+  See the configuration in the moduledoc for options shared between adapters,
+  for adapter-specific configuration see the adapter's documentation.
+  """
+  @callback start_link(opts :: Keyword.t) :: {:ok, pid} |
+                            {:error, {:already_started, pid}} |
+                            {:error, term}
+
+  @doc """
+  A callback executed when the repo starts or when configuration is read.
+
+  The first argument is the context the callback is being invoked. If it
+  is called because the Repo supervisor is starting, it will be `:supervisor`.
+  It will be `:dry_run` if it is called for reading configuration without
+  actually starting a process.
+
+  The second argument is the repository configuration as stored in the
+  application environment. It must return `{:ok, keyword}` with the updated
+  list of configuration or `:ignore` (only in the `:supervisor` case).
+  """
+  @callback init(:supervisor | :dry_run, config :: Keyword.t) :: {:ok, Keyword.t} | :ignore
+
+  @doc """
+  Shuts down the repository represented by the given pid.
+  """
+  @callback stop(pid, timeout) :: :ok
+
+  @doc """
+  """
+  @callback on_complete(id :: term) :: nil | no_return
 end
--- lib/rivulet/elastic_search_sink/supervisor.ex
+++ lib/rivulet/elastic_search_sink/supervisor.ex
@@ -1,15 +1,20 @@
 defmodule Rivulet.ElasticSearchSink.Supervisor do
+  @moduledoc false
   use Supervisor
-
-  require Logger
 
   alias Rivulet.ElasticSearchSink.Config
 
+  @defaults [timeout: 15000, pool_timeout: 5000]
+
+  @doc """
+  Starts the repo supervisor.
+  """
   def start_link(opts) do
-    Supervisor.start_link(__MODULE__, opts)
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(consumer_opts) do
+    # NOTE: instead of Config.from_sink_opts() it could be runtime_config() from below
     %Config{} = config = Config.from_sink_opts(consumer_opts)
 
     {:ok, _} = Application.ensure_all_started(:httpoison)
@@ -32,6 +37,16 @@ defmodule Rivulet.ElasticSearchSink.Supervisor do
     supervise(children, opts)
   end
 
+  @doc false
+  # def init(opts) do
+  #   children = [
+  #     supervisor(Task.Supervisor, [[name: job_supervisor()]]),
+  #     worker(Cronex.Table, [[scheduler: __MODULE__], [name: table()]])
+  #   ]
+  #
+  #   supervise(children, strategy: :one_for_one)
+  # end
+
   def find_manager(sink) do
     find_by_id(sink, :manager)
   end
@@ -52,4 +67,62 @@ defmodule Rivulet.ElasticSearchSink.Supervisor do
     |> Supervisor.which_children
     |> find_by_id(id)
   end
+
+  # def start_link(repo, otp_app, opts) do
+  #   name = Keyword.get(opts, :name, repo)
+  #   Supervisor.start_link(__MODULE__, {repo, otp_app, opts}, [name: name])
+  # end
+
+  # def init({repo, otp_app, opts}) do
+  #   case runtime_config(:supervisor, repo, otp_app, opts) do
+  #     {:ok, opts} ->
+  #       children = [adapter.child_spec(repo, opts)]
+  #       if Keyword.get(opts, :query_cache_owner, true) do
+  #         :ets.new(repo, [:set, :public, :named_table, read_concurrency: true])
+  #       end
+  #       supervise(children, strategy: :one_for_one)
+  #     :ignore ->
+  #       :ignore
+  #   end
+  # end
+
+  @doc """
+  Retrieves the runtime configuration.
+  """
+  def runtime_config(type, repo, otp_app, custom) do
+    if config = Application.get_env(otp_app, repo) do
+      config = [otp_app: otp_app, repo: repo] ++
+               (@defaults |> Keyword.merge(config) |> Keyword.merge(custom))
+
+      case repo_init(type, repo, config) do
+        {:ok, config} ->
+          {url, config} = Keyword.pop(config, :url)
+          {:ok, Keyword.merge(config, parse_url(url || ""))}
+        :ignore ->
+          :ignore
+      end
+    else
+      raise ArgumentError,
+        "configuration for #{inspect repo} not specified in #{inspect otp_app} environment"
+    end
+  end
+
+  defp repo_init(type, repo, config) do
+    if Code.ensure_loaded?(repo) and function_exported?(repo, :init, 2) do
+      repo.init(type, config)
+    else
+      {:ok, config}
+    end
+  end
+
+  @doc """
+  Retrieves the compile time configuration.
+  """
+  def compile_config(repo, opts) do
+    otp_app = Keyword.fetch!(opts, :otp_app)
+    config  = Application.get_env(otp_app, repo, [])
+
+    {otp_app, config}
+  end
+
 end
