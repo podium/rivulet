fcd6ba4b04c3efe3bc32341f288dbe3f56e2ca2c
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2018-04-04 11:19:14 -0500

    update

--- lib/rivulet/elastic_search_sink/config.ex
+++ lib/rivulet/elastic_search_sink/config.ex
@@ -21,8 +21,39 @@ defmodule Rivulet.ElasticSearchSink.Config do
     callback_module: any
   }
 
+  @doc """
+  NOTE: opts passed into from_sink_opts would look something
+  like this:
+
+    [
+     elastic_url: "http://elasticsearch:9200",
+     elastic_mapping: %{
+       properties: %{
+         _search: %{type: "text"},
+         adjusted_score: %{type: "short"},
+         conversation_uid: %{type: "keyword"},
+         created_at: %{format: "date_optional_time", type: "date"},
+         customer_name: %{copy_to: "_search", index: false, type: "text"},
+         customer_phone: %{type: "text"},
+         invitation_sent_at: %{format: "date_optional_time", type: "date"},
+         location_address: %{fields: %{raw: %{type: "keyword"}}, type: "text"},
+         location_name: %{fields: %{raw: %{type: "keyword"}}, type: "text"},
+         location_uid: %{type: "keyword"},
+         nps_comment: %{copy_to: "_search", index: "false", type: "text"},
+         nps_score: %{type: "short"},
+         organization_uid: %{type: "keyword"},
+         response_received_at: %{format: "date_optional_time", type: "date"}
+       }
+     },
+     otp_app: :snowden_sink,
+     callback_module: SnowdenSink.ElasticSearchSink,
+     elastic_index: "nps",
+     elastic_type: "response",
+     topic: "platform_nps_location_joins"
+   ]
+  """
   def from_sink_opts(opts) do
-    callback_module = Keyword.get(opts, :caller_module)
+    callback_module = Keyword.get(opts, :callback_module)
     elastic_index = Keyword.get(opts, :elastic_index)
     elastic_url = Keyword.get(opts, :elastic_url, [])
     elastic_type = Keyword.fetch!(opts, :elastic_type)
