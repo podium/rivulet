c48e607e668f385f888f162fd78caa8e2960f092
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2018-04-04 11:03:31 -0500

    change keys

--- lib/rivulet/elastic_search_sink/writer.ex
+++ lib/rivulet/elastic_search_sink/writer.ex
@@ -56,32 +56,32 @@ defmodule Rivulet.ElasticSearchSink.Writer do
     {:noreply, state}
   end
 
-  def filter_for_successfully_inserted(%{errors: false} = _es_response, records) do
+  def filter_for_successfully_inserted(%{"errors" => false} = _es_response, records) do
     Enum.map(records, &(&1.decoded_value))
   end
-  def filter_for_successfully_inserted(%{errors: true, items: es_items} = _es_response, records) do
+  def filter_for_successfully_inserted(%{"errors" => true, "items" => es_items} = _es_response, records) do
     zipped = Enum.zip(es_items, records)
     successful_msgs = Enum.filter(zipped, &message_successfully_inserted?/1)
     Enum.map(successful_msgs, fn ({_, %Message{decoded_value: val}}) -> val end)
   end
 
-  def message_successfully_inserted?({%{index: doc} = _es_resp, _}) do
+  def message_successfully_inserted?({%{"index" => doc} = _es_resp, _}) do
     status_ok?(doc)
   end
-  def message_successfully_inserted?({%{update: doc} = _es_resp, _}) do
+  def message_successfully_inserted?({%{"update" => doc} = _es_resp, _}) do
     status_ok?(doc)
   end
-  def message_successfully_inserted?({%{create: doc} = _es_resp, _}) do
+  def message_successfully_inserted?({%{"create" => doc} = _es_resp, _}) do
     status_ok?(doc)
   end
-  def message_successfully_inserted?({%{delete: doc} = _es_resp, _}) do
+  def message_successfully_inserted?({%{"delete" => doc} = _es_resp, _}) do
     status_ok?(doc)
   end
 
-  def status_ok?(%{status: status_code}) when status_code == 201 do
+  def status_ok?(%{"status" => status_code}) when status_code == 201 do
     true
   end
-  def status_ok?(%{status: status_code}) when status_code == 201 do
+  def status_ok?(%{"status" => status_code}) when status_code == 201 do
     false
   end
 
