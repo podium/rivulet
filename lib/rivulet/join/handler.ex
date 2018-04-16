42d024b5a75daa2052e55ed16b230cba99e33540
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2018-04-16 15:16:44 -0600

    final impl

--- lib/rivulet/join/handler.ex
+++ lib/rivulet/join/handler.ex
@@ -29,50 +29,12 @@ defmodule Rivulet.Join.Handler do
     {:ok, %State{join_id: join_id, transformers: transformers, consumer: consumer}}
   end
 
-  # pair join_keys with
-  @doc """
-  combo:
-  [
-  ---- these were the first to come in ---
-  {"b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
-   "b2f9bfea-3ed6-5915-9e97-60589c0b2a27"},
-  {"deccb141-6a3a-5127-8111-1d7936f985df",
-   "deccb141-6a3a-5127-8111-1d7936f985df"},
-  {"c0ee0235-5069-5773-b11e-280abca4bc20",
-   "c0ee0235-5069-5773-b11e-280abca4bc20"},
-  {"64ac0fd5-9f23-5d15-97c3-5d2c2086d763",
-   "64ac0fd5-9f23-5d15-97c3-5d2c2086d763"},
-  {"789686bf-fa4d-569a-9a9f-0fd66496fa48",
-   "789686bf-fa4d-569a-9a9f-0fd66496fa48"},
-  {"55471a1f-3f9a-55d2-90c3-8e04c1773617",
-   "55471a1f-3f9a-55d2-90c3-8e04c1773617"},
-  {"789686bf-fa4d-569a-9a9f-0fd66496fa48",
-   "63ea1ac2-28d4-50ed-8396-055f9326b380"},
-  {"c0ee0235-5069-5773-b11e-280abca4bc20",
-   "f0315c54-b272-5e93-86f9-9897c2d860c5"},
-  {"c0ee0235-5069-5773-b11e-280abca4bc20",
-   "f0315c54-b272-5e93-86f9-9897c2d860c5"},
-  {"b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
-   "49e32693-2ebf-5d61-8a52-c4a7086d7e19"},
-  {"b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
-   "49e32693-2ebf-5d61-8a52-c4a7086d7e19"}
-   --- these were the last to come in ---
-]
-  just join keys: ["b2f9bfea-3ed6-5915-9e97-60589c0b2a27", "deccb141-6a3a-5127-8111-1d7936f985df",
- "c0ee0235-5069-5773-b11e-280abca4bc20", "64ac0fd5-9f23-5d15-97c3-5d2c2086d763",
- "789686bf-fa4d-569a-9a9f-0fd66496fa48", "55471a1f-3f9a-55d2-90c3-8e04c1773617",
- "789686bf-fa4d-569a-9a9f-0fd66496fa48", "c0ee0235-5069-5773-b11e-280abca4bc20",
- "c0ee0235-5069-5773-b11e-280abca4bc20", "b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
- "b2f9bfea-3ed6-5915-9e97-60589c0b2a27"]
-  """
   def handle_call({:handle_resp, join_key_object_id_combo, ack_data}, from,  %State{join_id: join_id, transformers: transformers, consumer: consumer} = state) do
     GenServer.reply(from, :ok)
 
     just_join_keys = join_key_object_id_combo
       |> Enum.map(fn ({jk, _}) -> jk end)
       |> Enum.uniq
-
-    IO.inspect(just_join_keys, label: "just_join_keys unique")
 
     lookup_map = create_lookup_map(join_key_object_id_combo, just_join_keys)
 
@@ -101,7 +63,6 @@ defmodule Rivulet.Join.Handler do
 
         {join_key, decoded_docs, responsible}
       end)
-      |> IO.inspect(label: "res")
 
     Rivulet.Kafka.Join.Funcs.transforms(res, transformers)
 
@@ -126,19 +87,50 @@ defmodule Rivulet.Join.Handler do
   end
 
   @doc """
+  NOTE:
+  If 'combo' looks like this:
+  [
+    {"b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
+     "b2f9bfea-3ed6-5915-9e97-60589c0b2a27"},
+    {"deccb141-6a3a-5127-8111-1d7936f985df",
+     "deccb141-6a3a-5127-8111-1d7936f985df"},
+    {"c0ee0235-5069-5773-b11e-280abca4bc20",
+     "c0ee0235-5069-5773-b11e-280abca4bc20"},
+    {"64ac0fd5-9f23-5d15-97c3-5d2c2086d763",
+     "64ac0fd5-9f23-5d15-97c3-5d2c2086d763"},
+    {"789686bf-fa4d-569a-9a9f-0fd66496fa48",
+     "789686bf-fa4d-569a-9a9f-0fd66496fa48"},
+    {"55471a1f-3f9a-55d2-90c3-8e04c1773617",
+     "55471a1f-3f9a-55d2-90c3-8e04c1773617"},
+    {"789686bf-fa4d-569a-9a9f-0fd66496fa48",
+     "63ea1ac2-28d4-50ed-8396-055f9326b380"},
+    {"c0ee0235-5069-5773-b11e-280abca4bc20",
+     "f0315c54-b272-5e93-86f9-9897c2d860c5"},
+    {"c0ee0235-5069-5773-b11e-280abca4bc20",
+     "f0315c54-b272-5e93-86f9-9897c2d860c5"},
+    {"b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
+     "49e32693-2ebf-5d61-8a52-c4a7086d7e19"},
+    {"b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
+     "49e32693-2ebf-5d61-8a52-c4a7086d7e19"}
+  ]
+
+  'grouped' will look like this:
   %{
-  "55471a1f-3f9a-55d2-90c3-8e04c1773617" => ["55471a1f-3f9a-55d2-90c3-8e04c1773617"],
-  "64ac0fd5-9f23-5d15-97c3-5d2c2086d763" => ["64ac0fd5-9f23-5d15-97c3-5d2c2086d763"],
-  "789686bf-fa4d-569a-9a9f-0fd66496fa48" => ["789686bf-fa4d-569a-9a9f-0fd66496fa48",
-   "63ea1ac2-28d4-50ed-8396-055f9326b380"],
-  "b2f9bfea-3ed6-5915-9e97-60589c0b2a27" => ["b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
-   "49e32693-2ebf-5d61-8a52-c4a7086d7e19",
-   "49e32693-2ebf-5d61-8a52-c4a7086d7e19"],
-  "c0ee0235-5069-5773-b11e-280abca4bc20" => ["c0ee0235-5069-5773-b11e-280abca4bc20",
-   "f0315c54-b272-5e93-86f9-9897c2d860c5",
-   "f0315c54-b272-5e93-86f9-9897c2d860c5"],
-  "deccb141-6a3a-5127-8111-1d7936f985df" => ["deccb141-6a3a-5127-8111-1d7936f985df"]
-}
+    "55471a1f-3f9a-55d2-90c3-8e04c1773617" => ["55471a1f-3f9a-55d2-90c3-8e04c1773617"],
+    "64ac0fd5-9f23-5d15-97c3-5d2c2086d763" => ["64ac0fd5-9f23-5d15-97c3-5d2c2086d763"],
+    "789686bf-fa4d-569a-9a9f-0fd66496fa48" => ["789686bf-fa4d-569a-9a9f-0fd66496fa48",
+     "63ea1ac2-28d4-50ed-8396-055f9326b380"],
+    "b2f9bfea-3ed6-5915-9e97-60589c0b2a27" => ["b2f9bfea-3ed6-5915-9e97-60589c0b2a27",
+     "49e32693-2ebf-5d61-8a52-c4a7086d7e19",
+     "49e32693-2ebf-5d61-8a52-c4a7086d7e19"],
+    "c0ee0235-5069-5773-b11e-280abca4bc20" => ["c0ee0235-5069-5773-b11e-280abca4bc20",
+     "f0315c54-b272-5e93-86f9-9897c2d860c5",
+     "f0315c54-b272-5e93-86f9-9897c2d860c5"],
+    "deccb141-6a3a-5127-8111-1d7936f985df" => ["deccb141-6a3a-5127-8111-1d7936f985df"]
+  }
+
+  'grouped' has the same number of keys as will be present in the 'join_keys' list
+  passed in as second arg.
   """
   def create_lookup_map(combo, join_keys) do
     grouped = Enum.group_by(combo, &(elem(&1, 0)), &(elem(&1, 1)))
