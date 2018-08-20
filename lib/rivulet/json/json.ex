511e62ed9e206e323fee7a4fbe973df81457759e
Garrett Kirschbaum <garrett@podium.com>
2018-08-20 15:12:13 -0600

    fixed flap test

--- lib/rivulet/json/json.ex
+++ lib/rivulet/json/json.ex
@@ -1,8 +1,8 @@
 defmodule Rivulet.JSON do
-  @def encode(term) :: {:ok, String.t} | {:error, term}
+  @spec encode(term) :: {:ok, String.t} | {:error, term}
   def encode(data), do: handler().encode(data)
 
-  @def decode(String.t) :: {:ok, term} | {:error, term}
+  @spec decode(String.t) :: {:ok, term} | {:error, term}
   def decode(data), do: handler().decode(data)
 
   def handler(), do: Application.get_env(:rivulet, :json_handler) || Rivulet.JSON.Jiffy
--- test/json/json.exs
+++ test/json/json.exs
@@ -1,12 +1,25 @@
 defmodule Rivulet.JSON.Test do
   use ExUnit.Case
 
-  @test_module  Rivulet.JSON
+  @test_module Rivulet.JSON
 
-  describe "encode/1" do
-    test "if no json_handler is set it should default to jiffy" do
-      assert Rivulet.JSON.handler == :jiffy
-    end
+  defmodule TestJsonHandler do
+    def encode(data), do: {:ok, data.test}
+    def decode(data), do: {:ok, data.test}
   end
 
+  describe "handler/1" do
+    test "if no json_handler is set it should default to jiffy" do
+      assert @test_module.handler == Rivulet.JSON.Jiffy
+    end
+    test "should use test module to encode/decode json" do
+      Application.put_env(:rivulet, :json_handler, TestJsonHandler)
+      assert @test_module.encode(%{test: :test_encode}) == {:ok, :test_encode}
+      assert @test_module.decode(%{test: :test_decode}) == {:ok, :test_decode}
+      Application.put_env(:rivulet, :json_handler, Rivulet.JSON.Jiffy)
+    end
+    test "jiffy should encode correctly" do
+      assert @test_module.encode(%{test: :test_encode}) == {:ok, "{\"test\":\"test_encode\"}"}
+    end
+  end
 end
