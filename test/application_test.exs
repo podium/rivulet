f47ef91a960a65c05b8e795b664da386d4e0a57c
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2019-02-11 11:44:14 -0700

    update producer config to make it overrideable

---
+++ test/application_test.exs
@@ -0,0 +1,39 @@
+defmodule Rivulet.Application.Test do
+  use ExUnit.Case
+
+  alias Rivulet.Application
+
+  describe "kafka_producer_config/1" do
+    test "overrides defaults if custom configuration is provided" do
+      input = [
+        required_acks: 0
+      ]
+
+      expected = [
+        required_acks: 0,
+        ack_timeout: 10000,
+        max_retries: 30,
+        retry_backoff_ms: 1000
+      ]
+
+      result = Application.kafka_producer_config(input)
+
+      assert result == expected
+    end
+
+    test "falls back to defaults if custom configuration is not provided" do
+      input = []
+
+      expected = [
+        required_acks: 1,
+        ack_timeout: 10000,
+        max_retries: 30,
+        retry_backoff_ms: 1000
+      ]
+
+      result = Application.kafka_producer_config(input)
+
+      assert result == expected
+    end
+  end
+end
