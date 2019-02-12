0aa7e58347aa34f119f57be4e819b672e7e9c4ec
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2019-02-12 10:08:20 -0700

    update for when producer config is not defined

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,10 @@ config :rivulet,
   avro_schema_registry_uri: %URI{scheme: "http", host: "rivulet_schema-registry_1", port: 8081},
   kafka_brokers: [rivulet_kafka_1: 9092],
   client_name: :"rivulet-client-#{System.get_env("HOSTNAME")}",
-  json_handler: Rivulet.JSON.JiffyHandler
+  json_handler: Rivulet.JSON.JiffyHandler,
+  producer: [
+    required_acks: 0
+  ]
 
 # Configure elastix
 config :elastix,
