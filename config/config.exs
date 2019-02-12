955c9a3e13b3c91844726fe72483e19a909e8aec
Daniel Conger <danielconger@Daniels-MacBook-Pro-3.local>
2019-02-12 10:32:48 -0700

    remove config

--- config/config.exs
+++ config/config.exs
@@ -4,10 +4,7 @@ config :rivulet,
   avro_schema_registry_uri: %URI{scheme: "http", host: "rivulet_schema-registry_1", port: 8081},
   kafka_brokers: [rivulet_kafka_1: 9092],
   client_name: :"rivulet-client-#{System.get_env("HOSTNAME")}",
-  json_handler: Rivulet.JSON.JiffyHandler,
-  producer: [
-    required_acks: 0
-  ]
+  json_handler: Rivulet.JSON.JiffyHandler
 
 # Configure elastix
 config :elastix,
